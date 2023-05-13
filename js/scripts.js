f4324d4366835e285a368d1cad954c8927ad067d
joannawongww <joanna_1994@hotmail.com>
2023-05-13 13:46:06 +0800

    create pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//pokemon array
+let pokemonList = [
+    {name:'Bulbasaur', height:'0.7m', type:['Grass','Poison']}, 
+    {name:'Charmander', height:'0.6m', type:'Fire'}, 
+    {name:'Squirtle', height:'0.5m', type:'Water'},
+    {name:'Caterpie', height:'0.3m', type:'Bug'}
+];
